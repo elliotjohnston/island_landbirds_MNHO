f1ca5df8149c8893dec395cb78f83a88cc65133a
elliotjohnston <elliotjohnston21@gmail.com>
2023-10-27 18:19:59 -0400

    ARU_locations script

---
+++ scripts/ARU_locations.R
@@ -0,0 +1,10 @@
+########################################
+######### ARU Locations Script #########
+########################################
+# 
+# Date created: 10/27/2023
+# Created by: Elliot Johnston
+# 
+# Purpose: generate random coordinates for ARU deployment 
+
+# import island vectors
